eec9bf8959266648142b84b122757820cf4d5b96
LeinerViloria <vilorialeiner2002@gmail.com>
2022-01-29 12:25:07 -0500

    Comienza las instrucciones

---
+++ assets/js/game.js
@@ -0,0 +1,6 @@
+/**
+ * Se crean las constantes para las letras X y O
+ */
+
+const x = '<span class="equis letraPersonalizada">X</span>';
+const o = '<span class="circle letraPersonalizada">O</span>';
